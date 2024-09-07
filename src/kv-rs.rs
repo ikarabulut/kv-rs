9ec5afaf352b0a89f77894d25a3e6100c7ead9ff
Ismail Karabulut <i.karabulut93@gmail.com>
2024-09-07 13:13:23 -0500

    Add index

--- src/kv-rs.rs
+++ src/kv-rs.rs
@@ -1,4 +1,5 @@
 use kv_rs::KvStore;
+use std::collections::HashMap;
 
 #[cfg(target_os = "windows")]
 const USAGE: &str = "
@@ -18,7 +19,19 @@ Usage:
     kv_rs FILE update KEY VALUE
 ";
 
+type ByteStr = [u8];
+type ByteString = Vec<u8>;
+
+fn store_index_on_disk(a: &mut KvStore, index_key: &ByteStr) {
+    a.index.remove(index_key);
+    let index_as_bytes = bincode::serialize(&a.index).unwrap();
+    a.index = HashMap::new();
+    a.insert(index_key, &index_as_bytes).unwrap();
+}
+
 fn main() {
+    const INDEX_KEY: &ByteStr = b"+index";
+
     let args: Vec<String> = std::env::args().collect();
     let fname = args.get(1).expect(&USAGE);
     let action = args.get(2).expect(&USAGE).as_ref();
@@ -26,27 +39,42 @@ fn main() {
     let maybe_value = args.get(4);
 
     let path = std::path::Path::new(&fname);
-    let mut store = KvStore::open(path).expect("unable to open file");
-    store.load().expect("unable to load data");
+    let mut a = KvStore::open(path).expect("unable to open file");
+
+    a.load().expect("unable to load data");
 
     match action {
-        "get" => match store.get(key).unwrap() {
-            None => eprintln!("{:?} not found", key),
-            Some(value) => println!("{:?}", value),
-        },
+        "get" => {
+            let index_as_bytes = a.get(&INDEX_KEY)
+                .unwrap()
+                .unwrap();
 
-        "delete" => store.delete(key).unwrap(),
+            let index_decoded = bincode::deserialize(&index_as_bytes);
+
+            let index: HashMap<ByteString, u64> = index_decoded.unwrap();
+
+            match index.get(key) {
+                None => eprintln!("{:?} not found", key),
+                Some(&i) => {
+                    let kv = a.get_at(i).unwrap();
+                    println!("{:?}", kv.value)
+                }
+            }
+        }
+
+        "delete" => a.delete(key).unwrap(),
 
         "insert" => {
             let value = maybe_value.expect(&USAGE).as_ref();
-            store.insert(key, value).unwrap()
+            a.insert(key, value).unwrap();
+            store_index_on_disk(&mut a, INDEX_KEY);
         }
 
         "update" => {
             let value = maybe_value.expect(&USAGE).as_ref();
-            store.update(key, value).unwrap()
+            a.update(key, value).unwrap();
+            store_index_on_disk(&mut a, INDEX_KEY);
         }
-
         _ => eprintln!("{}", &USAGE),
     }
 }
